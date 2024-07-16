792ff80fc2c93a62ae4b32f39ad0f9fff6ac8bda
ViraBaldych <131186852+ViraBaldych@users.noreply.github.com>
2024-07-16 13:25:39 +0300

    Update mainpage.java

--- src/test/java/com/example/seleniumwithmurat/mainpage.java
+++ src/test/java/com/example/seleniumwithmurat/mainpage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class mainpage {
+public class MainPage {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
